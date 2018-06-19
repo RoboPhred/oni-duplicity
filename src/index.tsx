56b959499a287c6f50d047d1a2ff82b97759ee0b
RoboPhred <robophred@gmail.com>
2018-06-18 17:41:35 -0700

    [feat] Load a save and dump it on the screen

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-import Root from "./root";
-
-const rootEl = document.getElementById("root");
-
-ReactDOM.render(<Root />, rootEl);
--- src/root.tsx
+++
@@ -1,5 +0,0 @@
-import { hot } from "react-hot-loader";
-
-import App from "./components/App";
-
-export default hot(module)(App);
