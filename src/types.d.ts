6ee7ff91028bc37e3816fc6145e7ad2976daef81
RoboPhred <robophred@gmail.com>
2018-06-18 17:41:35 -0700

    [feat] Load a save and dump it on the screen

--- src/types.d.ts
+++ src/types.d.ts
@@ -9,3 +9,20 @@ declare module "worker-loader!*" {
 interface Window {
   __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: any;
 }
+
+declare module "react-files" {
+  export interface FilesProps {
+    className?: string;
+    onChange?(files: File[]): void;
+    onError?(error: Error, file: File): void;
+    accepts?: string[];
+    multiple?: boolean;
+    maxFiles?: number;
+    maxFileSize?: number;
+    minFileSize?: number;
+    clickable?: boolean;
+  }
+
+  const Files: React.ComponentClass<FilesProps>;
+  export default Files;
+}
