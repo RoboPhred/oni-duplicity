a5e9b0e310bb7ad23f97894fdc99df8783ef195a
RoboPhred <robophred@gmail.com>
2018-07-05 16:40:30 -0700

    Fix scroll issues due to EditorHeader margin

--- src/pages/SaveEditor/components/editors/components/EditorHeader.ts
+++ src/pages/SaveEditor/components/editors/components/EditorHeader.ts
@@ -1,5 +1,7 @@
 import Text from "@/components/Text";
 
-const EditorHeader = Text.withComponent("h2");
+const EditorHeader = Text.withComponent("h2").extend`
+  margin-top: 0;
+`;
 EditorHeader.displayName = "EditorHeader";
 export default EditorHeader;
