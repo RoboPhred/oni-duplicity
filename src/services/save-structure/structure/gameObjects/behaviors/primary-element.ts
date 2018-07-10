ec92670b770b07cdc84b6d33e32962d3fd0362f7
RoboPhred <robophred@gmail.com>
2018-07-09 22:53:19 -0700

    Fix PrimaryElementBehavior interface name

--- src/services/save-structure/structure/gameObjects/behaviors/primary-element.ts
+++ src/services/save-structure/structure/gameObjects/behaviors/primary-element.ts
@@ -1,11 +1,11 @@
-import { PrimaryElement, PrimaryElementBehavior } from "oni-save-parser";
+import { PrimaryElementBehavior } from "oni-save-parser";
 
 import { SaveStructureDef } from "../../types";
 import { behaviorIs } from "../../matchers";
 
 import { defaultBehavior } from "./default";
 
-export const primaryElementBehavior: SaveStructureDef<PrimaryElement> = {
+export const primaryElementBehavior: SaveStructureDef<PrimaryElementBehavior> = {
   ...defaultBehavior,
   $advanced: false,
 
