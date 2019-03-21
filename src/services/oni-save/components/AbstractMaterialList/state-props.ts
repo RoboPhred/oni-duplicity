4ee57366a452e6e4a6c9a9a64ff9f7979f2ad14a
William Matthews <william.matthews@webpt.com>
2019-03-21 12:37:50 -0700

    Reduce complexity of material list selector

--- src/services/oni-save/components/AbstractMaterialList/state-props.ts
+++ src/services/oni-save/components/AbstractMaterialList/state-props.ts
@@ -3,87 +3,132 @@ import {
   getBehavior,
   PrimaryElementBehavior,
   GameObject,
-  StorageBehavior
+  StorageBehavior,
+  GameObjectGroup
 } from "oni-save-parser";
 import { createSelector } from "reselect";
+import { values, orderBy, flatMap } from "lodash-es";
 
-import { saveGameSelector } from "../../selectors/save-game";
+import { AppState } from "@/state";
+
+import { gameObjectGroupsSelector } from "../../selectors/game-objects";
 
 import { MaterialListItem } from "./props";
-import { AppState } from "@/state";
 
 // TODO: Seeds, clothing, other sweepables
 const MaterialGameObjectTypes = [...SimHashNames];
 
 const materialsSelector = createSelector(
-  saveGameSelector,
-  saveGame => {
-    if (!saveGame) {
-      return [];
-    }
-
+  gameObjectGroupsSelector,
+  groups => {
     const rowsByMaterial: Record<string, MaterialListItem> = {};
 
-    function getMaterialRow(name: string) {
-      if (!rowsByMaterial[name]) {
-        rowsByMaterial[name] = {
-          name,
-          looseCount: 0,
-          looseGrams: 0,
-          storedCount: 0,
-          storedGrams: 0
-        };
-      }
-      return rowsByMaterial[name];
-    }
-
-    function addMaterialObject(
-      name: string,
-      gameObject: GameObject,
-      loose: boolean
-    ) {
-      const row = getMaterialRow(name);
-      const elementBehavior = getBehavior(gameObject, PrimaryElementBehavior);
-      if (!elementBehavior) {
-        return;
-      }
-
-      const { Units } = elementBehavior.templateData;
-      if (loose) {
-        row.looseCount++;
-        row.looseGrams += Units;
-      } else {
-        row.storedCount++;
-        row.storedGrams += Units;
+    if (groups) {
+      for (const group of groups) {
+        countGroupMaterials(group, rowsByMaterial);
       }
     }
 
-    for (const group of saveGame.gameObjects) {
-      const loose = MaterialGameObjectTypes.indexOf(group.name) !== -1;
-
-      for (const gameObject of group.gameObjects) {
-        if (loose) {
-          addMaterialObject(group.name, gameObject, true);
-        } else {
-          const storage = getBehavior(gameObject, StorageBehavior);
-          if (!storage) {
-            continue;
-          }
-          for (const item of storage.extraData) {
-            const primaryElement = getBehavior(item, PrimaryElementBehavior);
-            if (!primaryElement) {
-              continue;
-            }
-
-            addMaterialObject(item.name, gameObject, false);
-          }
-        }
-      }
-    }
-
-    return Object.keys(rowsByMaterial).map(k => rowsByMaterial[k]);
+    const rows = values(rowsByMaterial);
+    return orderBy(rows, ["name"]);
   }
 );
+
+function countGroupMaterials(
+  group: GameObjectGroup,
+  rowsByMaterial: Record<string, MaterialListItem>
+) {
+  const loose = isMaterialGameObject(group.name);
+  if (loose) {
+    countLooseMaterialGroup(group, rowsByMaterial);
+  } else {
+    countStorageGroup(group, rowsByMaterial);
+  }
+}
+
+function countLooseMaterialGroup(
+  group: GameObjectGroup,
+  rowsByMaterial: Record<string, MaterialListItem>
+) {
+  for (const gameObject of group.gameObjects) {
+    addMaterialObject(group.name, gameObject, true, rowsByMaterial);
+  }
+}
+
+function countStorageGroup(
+  group: GameObjectGroup,
+  rowsByMaterial: Record<string, MaterialListItem>
+) {
+  const storedObjects = flatMap(group.gameObjects, getStoredObjects);
+  for (const storedObject of storedObjects) {
+    const { type, gameObject } = storedObject;
+
+    if (!isMaterialGameObject(type)) {
+      continue;
+    }
+
+    addMaterialObject(type, gameObject, false, rowsByMaterial);
+  }
+}
+
+function getStoredObjects(
+  gameObject: GameObject
+): { type: string; gameObject: GameObject }[] {
+  const storage = getBehavior(gameObject, StorageBehavior);
+  if (!storage) {
+    return [];
+  }
+
+  return storage.extraData.map(({ name, ...gameObject }) => ({
+    type: name,
+    gameObject
+  }));
+}
+
+function getMaterialRow(
+  name: string,
+  rowsByMaterial: Record<string, MaterialListItem>
+) {
+  if (!rowsByMaterial[name]) {
+    rowsByMaterial[name] = {
+      name,
+      looseCount: 0,
+      looseGrams: 0,
+      storedCount: 0,
+      storedGrams: 0
+    };
+  }
+  return rowsByMaterial[name];
+}
+
+function isMaterialGameObject(type: string): boolean {
+  return MaterialGameObjectTypes.indexOf(type) !== -1;
+}
+
+function addMaterialObject(
+  name: string,
+  gameObject: GameObject,
+  loose: boolean,
+  rowsByMaterial: Record<string, MaterialListItem>
+) {
+  const elementBehavior = getBehavior(gameObject, PrimaryElementBehavior);
+  if (!elementBehavior) {
+    return;
+  }
+
+  const { Units } = elementBehavior.templateData;
+
+  // Use custom provided name, as many objects (food, clothing) have the same primary element.
+  const row = getMaterialRow(name, rowsByMaterial);
+
+  if (loose) {
+    row.looseCount++;
+    row.looseGrams += Units;
+  } else {
+    row.storedCount++;
+    row.storedGrams += Units;
+  }
+}
 
 export default function mapStateToProps(state: AppState) {
   return {
