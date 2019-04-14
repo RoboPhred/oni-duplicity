aea6feec1f2f549c172cf170672737ac23607595
RoboPhred <robophred@gmail.com>
2019-04-14 14:25:15 -0700

    Fix tests broken by new typescript version

--- src/__mocks__/active-state.ts
+++ src/__mocks__/active-state.ts
@@ -1,4 +1,3 @@
-import produce from "immer";
 
 import { AppState, defaultAppState } from "@/state";
 
@@ -6,9 +5,15 @@ import { LoadingStatus } from "@/services/oni-save/state";
 
 const saveGame = require("./save-game.json");
 
-const state: AppState = produce(defaultAppState, state => {
-  const oniSaveState = state.services.oniSave;
-  oniSaveState.loadingStatus = LoadingStatus.Ready;
-  oniSaveState.saveGame = saveGame;
-});
+const state: AppState = {
+  ...defaultAppState,
+  services: {
+    ...defaultAppState.services,
+    oniSave: {
+      ...defaultAppState.services.oniSave,
+      loadingStatus: LoadingStatus.Ready,
+      saveGame
+    }
+  }
+}
 export default state;
