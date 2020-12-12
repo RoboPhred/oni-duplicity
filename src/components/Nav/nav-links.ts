036fd3b336a78514738c39078d810517023f3e0b
RoboPhred <robophred@gmail.com>
2020-12-12 13:42:00 -0700

    Bump editor for save version 4.22 and disable planets editor

--- src/components/Nav/nav-links.ts
+++ src/components/Nav/nav-links.ts
@@ -9,43 +9,44 @@ const NavItems: NavItem[] = [
   {
     name: "Overview",
     path: "/",
-    i18nKey: "overview-page.title"
+    i18nKey: "overview-page.title",
   },
   {
     name: "Duplicants",
     path: "/duplicants",
     i18nKey: "duplicant.noun_titlecase_plural",
-    saveRequired: true
+    saveRequired: true,
   },
   {
     name: "Geysers",
     path: "/geysers",
     i18nKey: "geyser.noun_titlecase_plural",
-    saveRequired: true
+    saveRequired: true,
   },
-  {
-    name: "Planets",
-    path: "/planets",
-    i18nKey: "planet.noun_titlecase_plural",
-    saveRequired: true
-  },
+  // FIXME: Crashes post-dlc
+  // {
+  //   name: "Planets",
+  //   path: "/planets",
+  //   i18nKey: "planet.noun_titlecase_plural",
+  //   saveRequired: true
+  // },
   {
     name: "Materials",
     path: "/materials",
     i18nKey: "material.noun_titlecase_plural",
-    saveRequired: true
+    saveRequired: true,
   },
   {
     name: "Raw Editor",
     path: "/raw",
     i18nKey: "raw-editor-page.title",
-    saveRequired: true
+    saveRequired: true,
   },
   {
     name: "Changelog",
     path: "/changelog",
-    i18nKey: "changelog.title"
-  }
+    i18nKey: "changelog.title",
+  },
 ];
 
 export default NavItems;
